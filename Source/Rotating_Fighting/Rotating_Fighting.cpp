aaa8e14e2c81e768b6db83566980a003641b58b9
YevhenHoina <60106209+YevhenHoina@users.noreply.github.com>
2023-02-10 16:30:35 +0200

    Initial commit

---
+++ Source/Rotating_Fighting/Rotating_Fighting.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Rotating_Fighting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rotating_Fighting, "Rotating_Fighting" );
